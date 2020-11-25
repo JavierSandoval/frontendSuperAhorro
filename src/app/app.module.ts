96f8f36ce50b8dc39893ce415c665916b5f7af65
JavierSandoval <jrsandovalb@unal.edu.co>
2020-11-24 21:38:09 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { LayoutModule } from './layout/layout.module'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './componentes/paginas/inicio/inicio.component';
+import { CategoriasComponent } from './componentes/paginas/categorias/categorias.component';
+import { CarritoComponent } from './componentes/paginas/carrito/carrito.component';
+import { HistorialComprasComponent } from './componentes/paginas/historial-compras/historial-compras.component';
+import { ProcesarPagoComponent } from './componentes/paginas/procesar-pago/procesar-pago.component';
+import { RegistroComponent } from './componentes/usuario/registro/registro.component';
+import { InicioSesionComponent } from './componentes/usuario/inicio-sesion/inicio-sesion.component';
+import { ActualizarDatosComponent } from './componentes/usuario/actualizar-datos/actualizar-datos.component';
+import { AdministracionComponent } from './componentes/paginas/administracion/administracion.component';
+import { PaginaNoEncontradaComponent } from './componentes/paginas/pagina-no-encontrada/pagina-no-encontrada.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    CategoriasComponent,
+    CarritoComponent,
+    HistorialComprasComponent,
+    ProcesarPagoComponent,
+    RegistroComponent,
+    InicioSesionComponent,
+    ActualizarDatosComponent,
+    AdministracionComponent,
+    PaginaNoEncontradaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component'
+
+@NgModule ({
+    declarations: [
+        NavbarComponent,
+        FooterComponent
+    ],
+    exports:[
+        NavbarComponent,
+        FooterComponent
+    ]
+})
+export class LayoutModule {}
+
